d054e7e6ac4e51f8f48909b76f27b04f371adb07
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2015-03-07 23:15:07 +0200

    Add view for client list

--- vpersonna/vprofile/views.py
+++ vpersonna/vprofile/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 from vprofile.models import Client
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This if the vProfile start page.")
+    client_list = Client.objects.all()
+    template = loader.get_template('profile/index.html')
+    context = RequestContext(request, {
+        'client_list': client_list,
+        })
+    return HttpResponse(template.render(context))
 
 def client_profile(request, client_id):
     client = Client.objects.get(id=client_id)
