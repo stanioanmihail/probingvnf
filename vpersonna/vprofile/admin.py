b887fce4fff5701b8e6a42a2e96cf745f7c0550c
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2015-03-07 21:55:10 +0200

    Add vprofile models to admin screen

--- vpersonna/vprofile/admin.py
+++ vpersonna/vprofile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from vprofile.models import Client, Device, Remote, TrafficSession
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Device)
+admin.site.register(Remote)
+admin.site.register(TrafficSession)
