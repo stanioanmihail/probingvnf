949506e35aaa696566d9d94ed09767ecaba3c25a
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2015-03-07 22:08:11 +0200

    Add basic start page and URLs

--- vpersonna/vpersonna/urls.py
+++ vpersonna/vpersonna/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'vpersonna.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('vprofile.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
