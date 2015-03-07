1fd602ffe46d914fcc098bbc3e507ef6e4c471c9
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2015-03-07 23:06:04 +0200

    Add client profile view

--- vpersonna/vprofile/urls.py
+++ vpersonna/vprofile/urls.py
@@ -4,4 +4,5 @@ from vprofile import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<client_id>\d+)$', views.client_profile, name='profile'),
 )
--- vpersonna/vprofile/views.py
+++ vpersonna/vprofile/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from vprofile.models import Client
+
 # Create your views here.
 def index(request):
     return HttpResponse("This if the vProfile start page.")
+
+def client_profile(request, client_id):
+    client = Client.objects.get(id=client_id)
+    return HttpResponse("Profile page for client %s (id: %d)." %(client.client_name, client.client_id))
