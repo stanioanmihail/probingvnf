ec3573c2e1680c6011b1506c2871d0a9321e157a
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2015-03-08 03:38:44 +0200

    vprofile: Add proper protocol names for sample data in views

--- vpersonna/vprofile/views.py
+++ vpersonna/vprofile/views.py
@@ -17,7 +17,7 @@ def index(request):
 def client_profile(request, client_id):
     client = Client.objects.get(id=client_id)
     template = loader.get_template('profile/profile.html')
-    profile_dict = { 'a': 10, 'b': 20, 'c': 30, 'd': 15 }
+    profile_dict = { 'VoIP': 10, 'HTTP (non-video)': 20, 'BitTorrent': 30, 'Video': 15 }
     context = RequestContext(request, {
         'client': client,
         'profile_dict': profile_dict,
@@ -27,31 +27,31 @@ def client_profile(request, client_id):
 def profile_on_date(request, client_id):
     client = Client.objects.get(id=client_id)
     template = loader.get_template('profile/date.html')
-    tag_list = [ 'a', 'b', 'c', 'd' ]
+    tag_list = [ 'VoIP', 'HTTP (non-video)', 'BitTorent', 'Video' ]
     traffic_per_timeslot = {
             '08-10': {
-                'a': 10, 'b': 20, 'c': 30, 'd': 15
+                'VoIP': 10, 'HTTP (non-video)': 20, 'BitTorrent': 30, 'Video': 15
                 },
             '10-12': {
-                'a': 10, 'b': 20, 'c': 30, 'd': 15
+                'VoIP': 10, 'HTTP (non-video)': 20, 'BitTorrent': 30, 'Video': 15
                 },
             '12-14': {
-                'a': 5, 'b': 2, 'c': 20, 'd': 5
+                'VoIP': 5, 'HTTP (non-video)': 2, 'BitTorrent': 20, 'Video': 5
                 },
             '14-16': {
-                'a': 1, 'b': 0, 'c': 0, 'd': 1
+                'VoIP': 1, 'HTTP (non-video)': 0, 'BitTorrent': 0, 'Video': 1
                 },
             '16-18': {
-                'a': 3, 'b': 4, 'c': 4, 'd': 3
+                'VoIP': 3, 'HTTP (non-video)': 4, 'BitTorrent': 4, 'Video': 3
                 },
             '18-20': {
-                'a': 5, 'b': 2, 'c': 17, 'd': 20
+                'VoIP': 5, 'HTTP (non-video)': 2, 'BitTorrent': 17, 'Video': 20
                 },
             '20-22': {
-                'a': 5, 'b': 10, 'c': 30, 'd': 30
+                'VoIP': 5, 'HTTP (non-video)': 10, 'BitTorrent': 30, 'Video': 30
                 },
             '22-24': {
-                'a': 10, 'b': 5, 'c': 10, 'd': 15
+                'VoIP': 10, 'HTTP (non-video)': 5, 'BitTorrent': 10, 'Video': 15
                 },
             }
     context = RequestContext(request, {
