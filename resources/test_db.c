5eb30738d85e85f261eb6bdf8dd1230bd22cbdc4
Cristina Opriceana <cristina.opriceana@gmail.com>
2015-06-11 17:27:43 +0300

    DB: Insert info into database for a new client
    Signed-off-by: Cristina Opriceana <cristina.opriceana@gmail.com>

--- resources/test_db.c
+++ resources/test_db.c
@@ -7,35 +7,37 @@ static int callback(void *data, int argc, char **argv, char **azColName)
 	int i;
 
 	printf("argc is %d\n", argc);
-	for(i = 0; i<argc; i++){
-	
+	for(i = 0; i < argc; i++){
 		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
 	}
-
 }
 
-int main(){
-	
-	sqlite3 *db;
-	int rc;
-	char *sql;
+int main()
+{
+	char *sql_select, *sql_insert;
 	char *zErrMsg = 0;
 	const char *data = "Callback fct called\n";
+	sqlite3 *db;
+	int rc;
 
 	rc = sqlite3_open("db.sqlite3", &db);
 	if (rc) {
 		printf("Cannot open database\n");
 	}
 
-	sql = "SELECT * from vprofile_client;";
+	sql_select = "SELECT * from vprofile_client";
 
-	//sql = "SELECT name FROM db.sqlite3 WHERE type='table'";
-	//sql = "SELECT name FROM sqlite_temp_master WHERE type='table'";
-	
+	sql_insert = "INSERT INTO vprofile_client(name, email, address, phone_number, \
+		      card_id, contract_id, contract_type, username, password) VALUES\
+		      ('Cristina Georgiana', 'cristina.opriceana@yahoo.com', 'Str. X, Nr 29', \
+		       +123456789, 'ADASFEDSFDDFF', 'CONTR_3', 'CONTR', 'cristina.georgiana', 'test123');";
+
 	if (db == NULL) {
 		printf("DB IS NULL\n");
 	}
-	rc = sqlite3_exec(db, sql, callback, (void *) data, &zErrMsg);
+	//rc = sqlite3_exec(db, sql, callback, (void *) data, &zErrMsg);
+	rc = sqlite3_exec(db, sql_insert, callback, (void *) data, &zErrMsg);
+
 	if (rc) {
 		printf("Cannot execute query %d %s\n", rc, zErrMsg);
 	}
