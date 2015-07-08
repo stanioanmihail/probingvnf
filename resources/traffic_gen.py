13394a8cfb4441fba89ee98a80d7ae0ddbf42b4c
Cristina Opriceana <cristina.opriceana@gmail.com>
2015-07-08 19:20:42 +0300

    Generate torrent traffic in traffic_gen.py

--- resources/traffic_gen.py
+++ resources/traffic_gen.py
@@ -3,10 +3,25 @@ import sys
 
 # test
 for i in range(0, 100):
-    send(IP(src="10.0.99.100",dst="10.1.99.100")/ICMP()/"Hello World .flv .flv ")
+   send(IP(src="192.168.1.138",dst="10.1.99.100")/ICMP()/"Hello World .flv .flv ")
 
 # generate torrent traffic on ports
-
+'''
+for i in range(0, 50):
+    ip=IP(src="192.168.1.138")
+    ip.dst="192.168.0.2"
+    ip/TCP()
+    tcp=TCP(sport=9999, dport=9091)
+    (tcp/ip).show()
+    send(ip/tcp)
+'''
+for i in range(0, 50):
+    ip=IP(src="192.168.1.138")
+    ip.dst="192.168.0.2"
+    ip/TCP()
+    tcp=TCP(sport=9999, dport=6882)
+    (tcp/ip).show()
+    send(ip/tcp)
 
 # generate HTTP traffic on ports
 
